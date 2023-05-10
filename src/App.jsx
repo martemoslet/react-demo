d5b9d41b7a5d7376bf65a6d6fb0b064bb4d5738d
martemoslet <martemoslet@gmail.com>
2023-05-10 15:47:32 +0200

    Adds pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Header from "./components/ui/Header"
+import Footer from "./components/ui/Footer"
+import Products from './components/pages/Products';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Nav from './components/ui/Nav';
+import Product from './components/pages/Product';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Layout() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+      <div>
+        <Header bg="lavender" text="deeppink" padding="50px">
+          <h1>Site logo</h1>
+          <Nav />
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path='products' element={<Products />} />
+              <Route path='contact' element={<Contact />} />
+              <Route path="product/:id" element={<Product />} />
+            </Route>
+          </Routes>
+        </Header>
+        <Footer bg="LightCyan" text="DarkOrchid" padding="50px">
+          Copyright {(new Date()).getFullYear()}
+        </Footer>
+      </div>
+  )
+}
