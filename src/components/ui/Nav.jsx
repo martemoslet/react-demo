648c85636f1f5efc19e0f83d1e90f163e1ad518c
martemoslet <martemoslet@gmail.com>
2023-05-10 15:49:18 +0200

    Adds nav

---
+++ src/components/ui/Nav.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/products">Products</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/product/1">Product with ID: 1</Link>
+                </li>
+                <li>
+                    <Link to="/product/2">Product with ID: 2</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
