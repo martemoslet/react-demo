09f0978a37da34bcf0ce9d535744100b9c6485c8
martemoslet <martemoslet@gmail.com>
2023-05-10 15:48:33 +0200

    Adds products page

---
+++ src/components/pages/Products.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const url = 'https://api.noroff.dev/api/v1/online-shop';
+
+export default function Products() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch(url);
+            const json = await response.json();
+            setProducts(json);
+        }
+        getData();
+    }, []);
+
+    return (
+        <div>
+            {products.map((product) => (
+                <div>
+                    <h2>{product.title}</h2>
+                    <p>{product.description}</p>
+                    <p>Price: {product.price}</p>
+                    <p>Discounted price: {product.discountedPrice}</p>
+                </div>
+            ))}
+        </div>
+        );
+}
